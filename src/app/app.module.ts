68ce1b3032cbae13e60130feefd6e51671d3e460
Agustin Biagi <agustin.biagi@gmail.com>
2021-09-16 21:04:42 -0300

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
@@ -13,6 +14,7 @@ import { AppRoutingMogule } from './app-routing.modules';
   imports: [
     BrowserModule,
     AppRoutingMogule,
+	HttpClientModule,
     PaisModule,
     SharedModule
   ],
--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { PaisService } from '../../services/pais.service';
+
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
@@ -10,9 +12,12 @@ export class PorPaisComponent  {
 
 	termino :string = '';
 
-  constructor() { }
+	constructor( private paisService: PaisService) { }
 
 	buscar() {
 		console.log(this.termino);
+
+		this.paisService.buscarPais(this.termino).subscribe();
+
 	}
 }
