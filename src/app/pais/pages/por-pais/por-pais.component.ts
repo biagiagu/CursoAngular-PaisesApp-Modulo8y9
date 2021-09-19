295022d8f98599f1ad87fb06d6d23824e9564d14
Agustin Biagi <agustin.biagi@gmail.com>
2021-09-18 22:37:56 -0300

    .

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -11,13 +11,20 @@ import { PaisService } from '../../services/pais.service';
 export class PorPaisComponent  {
 
 	termino :string = '';
+	hayError: boolean=false;
+
 
 	constructor( private paisService: PaisService) { }
 
 	buscar() {
-		console.log(this.termino);
+		this.hayError=false;
+		//console.log(this.termino);
 
-		this.paisService.buscarPais(this.termino).subscribe();
+		this.paisService.buscarPais(this.termino).subscribe(resp =>{
+			console.log(resp);
+		},(err)=>{
+			this.hayError=true
+		});
 
 	}
 }
